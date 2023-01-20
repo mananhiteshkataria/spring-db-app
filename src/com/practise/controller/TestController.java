7cd27d0e89cbaa01b5435cfb88f93c1483d18a71
mananhiteshkataria <47041531+mananhiteshkataria@users.noreply.github.com>
2023-01-20 13:45:18 +0530

    added all derby dependencies
    check pom.xml i have added 3 derby dependencies if you are using above derby 10.15 you must have to do this.

---
+++ src/com/practise/controller/TestController.java
@@ -0,0 +1,20 @@
+package com.practise.controller;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.practise.service.EmployeeService;
+
+public class TestController {
+public static void main(String[] args) {
+	ApplicationContext ctx= new ClassPathXmlApplicationContext("beans.xml");
+	// user service object and perform crud operation
+	
+	EmployeeService service=(EmployeeService)ctx.getBean("employeeService");
+	
+//	// service needs to have methods
+//	ClassPathXmlApplicationContext ctax = (ClassPathXmlApplicationContext)ctx;
+//	ctax.close();
+	
+}
+}
