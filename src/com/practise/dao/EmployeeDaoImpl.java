6bc146d5edcebadc9c9052c9893d7d2b33a4cfbe
mananhiteshkataria <47041531+mananhiteshkataria@users.noreply.github.com>
2023-01-20 12:39:21 +0530

    Update EmployeeDaoImpl.java
    I made template to inject the made jdbc template

--- src/com/practise/dao/EmployeeDaoImpl.java
+++ src/com/practise/dao/EmployeeDaoImpl.java
@@ -1,9 +1,17 @@
 package com.practise.dao;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+
 import com.practise.beans.Employee;
 
 // we need to implement these methods
 public class EmployeeDaoImpl implements EmployeeDao {
+	
+	private JdbcTemplate template; // make setter method
+	// setter method to inject JdbcTemplate Object
+	public void setTemplate(JdbcTemplate template) {
+		this.template = template;
+	}
 
 	@Override
 	public int save(Employee employee) {
